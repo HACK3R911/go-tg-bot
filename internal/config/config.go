39160b445d4015afcada72eb5ac49f078c277758
HACK3R911 <afanasevevgenij2004@gmail.com>
2026-02-24 16:01:12 +0300

    fix: improved handling of env files for dokcer compose

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,17 +1,36 @@
 package config
 
 import (
-	"github.com/joho/godotenv"
 	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func Load(path string) error {
-	err := godotenv.Load(path)
-	if err != nil {
-		log.Fatal("Ошибка загрузки .env файла")
-		return err
+	if path == "" || path == ".env" {
+		if _, err := os.Stat(".env"); os.IsNotExist(err) {
+			if os.Getenv("TELEGRAM_BOT_TOKEN") != "" {
+				log.Println("Using environment variables (no .env file found)")
+				return nil
+			}
+		}
 	}
+
+	if path != "" && path != ".env" || fileExists(path) {
+		err := godotenv.Load(path)
+		if err != nil {
+			log.Printf("Ошибка загрузки .env файла: %v", err)
+			return err
+		}
+	}
+
 	return nil
+}
+
+func fileExists(path string) bool {
+	_, err := os.Stat(path)
+	return err == nil
 }
 
 type APIConfig interface {
